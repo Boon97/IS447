c1daa13c026cdd946bd8e6e9a09f8e91c83910e4
Justin <80321656+justinkan420@users.noreply.github.com>
2022-02-21 18:41:21 +0800

    REWORK
    'login.html' is now the default homepage 
    logging in takes you to the calendar page
    navbar removed from login page
    navbar added only after you login
    navbar streamlined to include only 'Profile' 'Calendar' 'Logout'
    logout takes you back to the login page

---
+++ website/auth.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/') # login page when you enter the site
+def index():
+	return render_template('login.html')
+
+@auth.route('/login', methods=['POST', 'GET']) # POST request to authenticate user
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        return redirect(url_for('auth.calendar', usr=user)) # routes to calendar of user upon successful authentication
+    else: 
+        return render_template('login.html')
+
+@auth.route('/<usr>') # should be routed properly to user's profile
+def user(usr):
+    return render_template('profile.html')
+
+@auth.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@auth.route('/signup') 
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/calendar')
+def calendar():
+    return render_template('calendar_events.html')
+
