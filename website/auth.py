4acf2e7d230f9d3c8d9d8ef5fe3f04da327e6e1f
ZhengJie98 <zjong.2019@smu.edu.sg>
2022-03-05 15:56:19 +0800

    finished login and password

--- website/auth.py
+++ website/auth.py
@@ -18,35 +18,61 @@ def index():
 def login():
     if request.method == 'POST':        
         user = request.form['username']
-        session['user'] = user # storing information in the session
+        password = request.form['password']
+        # print(user)
+        # print(password)
+       
         print("Username is :", user)
 
-        
+        ## CONNECT DATABASE     
         currentdirectory = os.path.dirname(os.path.abspath(__file__))
-        print("NOW WE ARE IN:" , currentdirectory)
-        print("HELLLLLO======================")
+        # print("NOW WE ARE IN:" , currentdirectory)
         connect_directory = currentdirectory + "\pythonsqlite.db"
         print(connect_directory)
         connection = sqlite3.connect(connect_directory)
         cursor = connection.cursor()
-        query1 = "SELECT * FROM employee_details WHERE employee_name = ?"
-        # query1 = "SELECT * FROM employee_details WHERE employee_email = ?"
-        # user_email = "zjong.2019@scis.smu.edu.sg"
-        # query1 = "SELECT * FROM employee_details"
-        print(query1)
-        result = cursor.execute(query1, (user,))
-        rows = result.fetchall()
-        for row in rows:
-            print(row)
+        
 
+        ## CHECK IF USERNAME EXIST
+        query = "SELECT EXISTS (SELECT * FROM employee_details WHERE employee_name=?)"
+        result = cursor.execute(query, (user,))
+        row = result.fetchall()
+        value_exist = row[0][0]
+        # print(value_exist)
 
-        flash('Login Successful!')
-        return redirect(url_for('auth.profile', user=user)) # routes to calendar of user upon successful authentication
-    elif 'user' in session:
-        flash('Already Logged In!')
-        return redirect(url_for('auth.profile'))
-    else:    
-        return render_template('login.html')
+        if (value_exist == 1):
+
+            ## CHECK IF PASSWORD IS SAME
+            query1 = "SELECT employee_password FROM employee_details WHERE employee_name = ?"
+            # query1 = "SELECT * FROM employee_details WHERE employee_email = ?"
+            # user_email = "zjong.2019@scis.smu.edu.sg"
+            # print(query1)
+            result = cursor.execute(query1, (user,))
+            row = result.fetchall()
+            password_returned = row[0][0]
+            # print(password_returned)
+
+            if (password == password_returned):
+                flash('Login Successful!')
+                session['user'] = user # storing information in the session
+                
+                return render_template('profile.html', user=user)
+                # return redirect(url_for('auth.profile', user=user)) # routes to calendar of user upon successful authentication
+            
+            else:
+                flash("wrong user_id or password")
+                return render_template('login.html')
+        
+        ## ASK JUSTIN WHATS THIS
+        # elif 'user' in session:
+        #     flash('Already Logged In!')
+        #     return redirect(url_for('auth.profile'))
+
+        ## If user_id doesn't exist, go back login.html
+        else:    
+            flash("wrong user_id or password")
+            return render_template('login.html')
+
 
 
 
