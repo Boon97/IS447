982c760327d0dd58736be5c8496dbef282978e33
ZhengJie98 <zjong.2019@smu.edu.sg>
2022-03-10 12:25:35 +0800

    changed_to_roles_count

--- website/static/js/calendar_events_2.js
+++ website/static/js/calendar_events_2.js
@@ -1,6 +1,8 @@
 let nav = 0;
 let clicked = null;
-let events = seperated_into_each_day // From calendar.html, an object of individual dates
+// let events = seperated_into_each_day // From calendar.html, an object of individual dates
+let events = approved_total_leave_by_roles_obj
+console.log(events)
 
 const calendar = document.getElementById('calendar');
 const newEventModal = document.getElementById('newEventModal');
@@ -29,6 +31,7 @@ function openModal(date) {
 
   backDrop.style.display = 'block';
 }
+
 
 function load() {
   const dt = new Date();
@@ -75,7 +78,7 @@ function load() {
           events_for_day_array.push(each_event)
         }
       }
-      // console.log(events_for_day_array)
+      console.log(events_for_day_array)
 
       if (i - paddingDays === day && nav === 0) {
         daySquare.id = 'currentDay';
@@ -95,13 +98,27 @@ function load() {
       if (events_for_day_array.length > 0 ) {
         for (each_event of events_for_day_array){
           // console.log(each_event)
-          const eventDiv = document.createElement('div');
-          // console.log('eventDiv: ', eventDiv)
-          eventDiv.classList.add('event');
-          eventDiv.innerText = each_event.title;
-          // console.log('eventForDay.title: ', each_event.title)
+          if (each_event.consultant_count > 0){
+            var eventDiv = document.createElement('div');
+            eventDiv.classList.add('event');
+            eventDiv.innerText = "CT: " + each_event.consultant_count;
+            daySquare.appendChild(eventDiv);
+          }
 
-          daySquare.appendChild(eventDiv);
+          if (each_event.registrar_count > 0){
+            var eventDiv = document.createElement('div');
+            eventDiv.classList.add('event');
+            eventDiv.innerText = "RS: " + each_event.registrar_count;
+            daySquare.appendChild(eventDiv);
+          }
+          
+          if (each_event.medical_officer_count > 0){
+            var eventDiv = document.createElement('div');
+            eventDiv.classList.add('event');
+            eventDiv.innerText = "MO: " + each_event.medical_officer_count;
+            daySquare.appendChild(eventDiv);
+          }
+          
           event_index += 1
           // console.log("event_index: ", event_index)
         
@@ -117,6 +134,94 @@ function load() {
     calendar.appendChild(daySquare);    
   }
 }
+
+// function load() {
+//   const dt = new Date();
+
+//   if (nav !== 0) {
+//     dt.setMonth(new Date().getMonth() + nav);
+//   }
+
+//   const day = dt.getDate();
+//   const month = dt.getMonth();
+//   const year = dt.getFullYear();
+
+//   const firstDayOfMonth = new Date(year, month, 1);
+//   const daysInMonth = new Date(year, month + 1, 0).getDate();
+  
+//   const dateString = firstDayOfMonth.toLocaleDateString('en-GB', {
+//     weekday: 'long',
+//     year: 'numeric',
+//     month: 'numeric',
+//     day: 'numeric',
+//   });
+//   const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+//   document.getElementById('monthDisplay').innerText = 
+//     `${dt.toLocaleDateString('en-GB', { month: 'long' })} ${year}`;
+
+//   calendar.innerHTML = '';
+
+//   for(let i = 1; i <= paddingDays + daysInMonth; i++) {
+//     const daySquare = document.createElement('div');
+//     daySquare.classList.add('day');
+//     var events_for_day_array = []
+//     const dayString = `${i - paddingDays}/${month + 1}/${year}`;
+//     // console.log("daystring: ", dayString)
+
+//     if (i > paddingDays) {
+//       // console.log('paddingDays: ', paddingDays)
+//       daySquare.innerText = i - paddingDays;
+//       const eventForDay = events.find(e => e.date === dayString);
+//       // console.log("eventForDay: ", eventForDay)
+//       for (each_event of events){
+//         // console.log(each_event)
+//         if (each_event.date == dayString){
+//           events_for_day_array.push(each_event)
+//         }
+//       }
+//       // console.log(events_for_day_array)
+
+//       if (i - paddingDays === day && nav === 0) {
+//         daySquare.id = 'currentDay';
+//         // console.log('daysquare: ', daySquare)
+//       }
+
+//       // if (eventForDay) {
+//       //   const eventDiv = document.createElement('div');
+//       //   console.log('eventDiv: ', eventDiv)
+//       //   eventDiv.classList.add('event');
+//       //   eventDiv.innerText = eventForDay.title;
+//       //   console.log('eventForDay.title: ', eventForDay.title)
+
+//       //   daySquare.appendChild(eventDiv);
+//       // }
+//       var event_index = 0
+//       if (events_for_day_array.length > 0 ) {
+//         for (each_event of events_for_day_array){
+//           // console.log(each_event)
+//           const eventDiv = document.createElement('div');
+//           // console.log('eventDiv: ', eventDiv)
+//           eventDiv.classList.add('event');
+//           eventDiv.innerText = each_event.title;
+//           // console.log('eventForDay.title: ', each_event.title)
+
+//           daySquare.appendChild(eventDiv);
+//           event_index += 1
+//           // console.log("event_index: ", event_index)
+        
+//         }
+//         }
+
+      
+//       daySquare.addEventListener('click', () => openModal(dayString));
+//     } else {
+//       daySquare.classList.add('padding');
+//     }
+
+//     calendar.appendChild(daySquare);    
+//   }
+// }
 
 function closeModal() {
   eventTitleInput.classList.remove('error');
